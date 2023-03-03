1ed816840d7573800889806f91ec25e6d7a0ddee
Tofaa <82680183+Tofaa2@users.noreply.github.com>
2023-03-04 02:17:39 +0400

    make it not polaroid dependant

--- src/main/java/net/polar/PCommands.java
+++ src/main/java/net/polar/PCommands.java
@@ -1,11 +1,14 @@
 package net.polar;
 
 import net.kyori.adventure.text.Component;
+import net.minestom.server.MinecraftServer;
 import net.minestom.server.extensions.Extension;
 import net.polar.commands.FindCommand;
 import net.polar.commands.GamemodeCommand;
 import net.polar.commands.TeleportCommand;
 import net.polar.utils.ChatColor;
+
+import java.util.List;
 
 public class PCommands extends Extension {
 
@@ -15,11 +18,9 @@ public class PCommands extends Extension {
 
     @Override
     public void initialize() {
-        Polaroid.registerCommands(
-                new GamemodeCommand(),
+        List.of(new GamemodeCommand(),
                 new TeleportCommand(),
-                new FindCommand()
-        );
+                new FindCommand()).forEach(MinecraftServer.getCommandManager()::register);
     }
 
     @Override
